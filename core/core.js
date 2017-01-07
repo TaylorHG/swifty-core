75000f6b8d9b03a3427786bf13a9b491c6efae7d
harbinger <taylor@hgdata.com>
2017-01-06 22:54:37 -0800

    fix issues with importing modules

--- core/core.js
+++ core/core.js
@@ -4,14 +4,15 @@ export default class SwiftyCore {
   constructor() {
     console.log('Spawning Application...');
     this.httpHandler = express();
-    app.all(/.*/, function(req, res, next) {
+    this.httpHandler.all(/.*/, function(req, res, next) {
       console.log('request recieved!');
       // use SwiftResolver to find or create modules to handle the request.
       res.send("Here's a response!");
     });
-    app.listen(1337, function () {
+    this.httpHandler.listen(1337, function () {
       console.log('Started listening on port 1337...');
     });
     console.log('Application Spawned! Listening for requests...\n\n\n');
   }
 }
+export var __useDefault = true;
--- index.js
+++
@@ -1,4 +0,0 @@
-var Core = require('./distribution/core.js');
-
-// Export Core module after it has been compiled by Babel.
-module.exports = Core;
