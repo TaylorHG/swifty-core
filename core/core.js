c4795aea6023fcb8fdcc8a044290c470b4db3445
harbinger <taylor@hgdata.com>
2017-01-09 23:03:19 -0800

    abstract AppWatcher into it's own class

--- core/core.js
+++ core/core.js
@@ -1,12 +1,10 @@
 import connect from 'connect';
 import http from 'http';
+import SwiftyObject from '@swift-developer/swifty-objects';
 
 import Resolver from './resolver/resolver';
 import RequestHandler from './request-handler';
-import SwiftyObject from '@swift-developer/swifty-objects';
-
-import watch from 'watch';
-import colors from 'colors/safe';
+import AppWatcher from './app-watcher';
 
 export default class SwiftyCore extends SwiftyObject {
   constructor() {
@@ -35,34 +33,8 @@ export default class SwiftyCore extends SwiftyObject {
       // create request handler
       var requestHandler = new RequestHandler(_this.get('resolver'), _this.get('httpManager'));
 
-      console.log(`Start watching files in "${process.cwd()}/app" for updates...`);
-      // start watching files and compile them as you go.
-      watch.createMonitor(`${process.cwd()}/app`, { interval: 0.1 }, function (monitor) {
-        monitor.on("created", function (f, stat) {
-          // Handle new files
-          var regex = new RegExp("^" + `${process.cwd()}/app`);
-          console.log(colors.green('[+] created new file: ') + f.replace(regex, ''));
-          _this.get('resolver').addLayerByFileName(f).then(function(layerContainer) {
-            console.log(`Layer registered: ${layerContainer.layerKey.type}:${layerContainer.layerKey.name}\n`);
-          });
-        })
-        monitor.on("changed", function (f, curr, prev) {
-          // Handle file changes
-          var regex = new RegExp("^" + `${process.cwd()}/app`);
-          console.log(colors.blue('[~] changed file: ') + f.replace(regex, ''));
-          _this.get('resolver').addLayerByFileName(f).then(function(layerContainer) {
-            console.log(`Layer re-registered: ${layerContainer.layerKey.type}:${layerContainer.layerKey.name}\n`);
-          });
-        })
-        monitor.on("removed", function (f, stat) {
-          // Handle removed files
-          var regex = new RegExp("^" + `${process.cwd()}/app`);
-          console.log(colors.red('[-] removed file: ') + f.replace(regex, ''));
-          _this.get('resolver').removeLayerByFileName(f).then(function(layerContainer) {
-            console.log(`Layer de-registered: ${layerContainer.layerKey.type}:${layerContainer.layerKey.name}\n`);
-          });
-        })
-      })
+      // watch files for changes and load changes into resolver
+      new AppWatcher(_this.get('resolver')).watchApp();
       console.log('Application Built!\n\n\n');
 
       // configure connect.js to respond to all requests using the request handler
