9d4b32a07d025b7affe40efca459a6f8d384a832
harbinger <taylor@hgdata.com>
2017-04-04 21:26:46 -0700

    things

--- test-app/resources/application.js
+++ test-app/resources/application.js
@@ -1,16 +1,17 @@
 import Resource from '../../../distribution/common-layers/resource';
 import { get } from '@swift-developer/swifty-rest';
+import { LOGGER } from '@swift-developer/swifty-logger';
 
 export default class ApplicationResource extends Resource {
 
   @get()
   index(req, res) {
-    console.log('application route index hit!');
+    LOGGER.info('application route index hit!');
   }
 
   @get('/:id')
   show(req, res) {
-    console.log('application route show hit!');
+    LOGGER.info('application route show hit!');
   }
 
 }
--- test-app/router.js
+++ test-app/router.js
@@ -3,7 +3,7 @@ import Router from '../../distribution/common-layers/router';
 export default class ApplicationRouter extends Router {
   map() {
     this.route('/', { resource: 'application' }, function() { // creating a root route which all requests will pass through
-      this.route('swags'); // in this case the "swags" resource will be used, (defaults to path name)
+      this.route('swags', { resource: 'swags' }); // in this case the "swags" resource will be used, (defaults to path name)
       this.route('yolos', { resource: 'yolos' }); // in this case the "yolos" resource will be used
     });
   }
